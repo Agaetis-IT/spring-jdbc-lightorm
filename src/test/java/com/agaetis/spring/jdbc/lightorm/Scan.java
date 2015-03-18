1a5502454ff465e7ec42a02a96579894c7523576
Mickaël GERVAIS <mgervais@agaetis.fr>
2015-03-18 14:17:51 +0100

    Use a class for component scan, instead of hard coded package name.

---
+++ src/test/java/com/agaetis/spring/jdbc/lightorm/Scan.java
@@ -0,0 +1,13 @@
+package com.agaetis.spring.jdbc.lightorm;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * No-op marker that serves no purpose other than being used by
+ * {@link ComponentScan#basePackageClasses()}
+ *
+ * @author mgervais
+ */
+public final class Scan {
+
+}
--- src/test/java/com/agaetis/spring/jdbc/lightorm/test/AbstractTest.java
+++ src/test/java/com/agaetis/spring/jdbc/lightorm/test/AbstractTest.java
@@ -1,5 +1,7 @@
 package com.agaetis.spring.jdbc.lightorm.test;
 
+import javax.sql.DataSource;
+
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,34 +13,31 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.sql.DataSource;
+import com.agaetis.spring.jdbc.lightorm.Scan;
 
 /**
- * Created by <a href="https://github.com/rnicob">Nicolas Roux</a> - <a href="http://www.agaetis.fr">Agaetis</a>  on 12/03/2015.
+ * Created by <a href="https://github.com/rnicob">Nicolas Roux</a> - <a
+ * href="http://www.agaetis.fr">Agaetis</a> on 12/03/2015.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public abstract class AbstractTest {
 
-    @Configuration
-    @ComponentScan(basePackages = {"com.agaetis.spring.jdbc.lightorm"})
-    static class Config {
+	@Configuration
+	@ComponentScan(basePackageClasses = Scan.class)
+	static class Config {
 
-        @Bean
-        public static PropertySourcesPlaceholderConfigurer properties() throws Exception {
-            final PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-            pspc.setLocation(new ClassPathResource("/test.properties"));
-            return pspc;
-        }
+		@Bean
+		public static PropertySourcesPlaceholderConfigurer properties() throws Exception {
+			final PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+			pspc.setLocation(new ClassPathResource("/test.properties"));
+			return pspc;
+		}
 
-        @Bean
-        public DataSource dataSource() {
-            return new EmbeddedDatabaseBuilder()
-                    .setType(EmbeddedDatabaseType.H2)
-                    .addScript("classpath:schema.sql")
-                    .addScript("classpath:test-data.sql")
-                    .build();
-        }
-    }
+		@Bean
+		public DataSource dataSource() {
+			return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("classpath:schema.sql").addScript("classpath:test-data.sql").build();
+		}
+	}
 
 }
